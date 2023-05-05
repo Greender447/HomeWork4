61c5e96236a3de400347b01638a6c93d67e0b4b1
Grigory_Uyranev <23grisha23okay1@mail.ru>
2023-05-05 11:22:17 +0300

    HomeWork4.3

---
+++ homework4.3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов (от -10 до 10) и выводит их на экран.
+
+// 1, 2, 5, 7, 9 -> [1, 2, 5, 7, 9]
+
+// 6, 1, 3 -> [6, 1, 3]
+
+Console.WriteLine("Ввtдите длину массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+RandNumberArray(array);
+PrintArray(array);
+void RandNumberArray(int[] array)
+{
+    for (int i = 0; i < size; i++)
+    {
+         array[i] = new Random().Next(-9, 9);
+
+    }
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
